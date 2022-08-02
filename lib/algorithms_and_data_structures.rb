135b9bef2984e18da8d99d6bb99bceadb48f37c9
JCPedroza <nemesis4go10@hotmail.com>
2022-08-01 19:33:39 -0500

    initial project commit

---
+++ lib/algorithms_and_data_structures.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "algorithms_and_data_structures/version"
+
+module AlgorithmsAndDataStructures
+  class Error < StandardError; end
+  # Your code goes here...
+end
